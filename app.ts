b8cf0f5cd120922cfc7307dfce4e8e72422ab64c
UHyun-K <74442715+UHyun-K@users.noreply.github.com>
2021-11-05 00:22:16 +0900

    type declaration
    varible, function type declaration

--- app.ts
+++ app.ts
@@ -1,34 +1,69 @@
+type Score = {
+  currentPage: number;
+  feeds: NewsFeed[]; //뉴스피드의 타입이 들억나ㅡㄴ 배열 
+}
+type News ={
+  id: number;
+  time_ago: string;
+  title: string;
+  url: string;
+  user: string;
+  content: string;
 
-const ajax = new XMLHttpRequest(); //해커뉴스데이터 가져올 도구 가져오기, 저장하기
-const container = document.getElementById("root");
+}
+type NewsFeed = News & {
+  point: number;
+  comments_count : number;
+  read?: boolean; //처음에는 데이터가 없다가 나중ㅇ 추가하는 거라
+}
+
+type NewsDetail = News &{
+  comments:NewsComment[];
+}
+
+type NewsComment = News &{
+  comments:NewsComment[];
+  level:number;
+}
+
+const ajax: XMLHttpRequest = new XMLHttpRequest(); //해커뉴스데이터 가져올 도구 가져오기, 저장하기
+const container: HTMLElement | null = document.getElementById("root");
 const NEWS_URL =  'https://api.hnpwa.com/v0/news/1.json';
 const CONTENTS_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 
-const store = {
+const store:Score = {
   currentPage: 1,
   feeds:[],
 };
 
-function getData(url){
+function getData<ajaxResponse>(url:string):ajaxResponse{
   ajax.open('GET', url, false);
   ajax.send();
 
   return JSON.parse(ajax.response);
 }
 
-function makeFeeds(feeds){ //getdata에 속성 추가 할 용도인데 getData는 자주쓰이니까
+function makeFeeds(feeds:NewsFeed[]):NewsFeed[]{ //getdata에 속성 추가 할 용도인데 getData는 자주쓰이니까
   for(let i =0; i < feeds.length; i++){
     feeds[i].read =false; //처음엔 다 안읽었으니까
   }
   return feeds;
 }
 
-function newsFeed(){
-let newsFeed = store.feeds;
+
+function updateView(html:string):void{ //타입가드 들어오는 코드 유형이 2가지일때 
+  if(container != null){
+    container.innerHTML= html;
+  }else{
+    console.log('최상위 컨테이너가 없어 ui진행하지 못함');
+  }
+}
+function newsFeed():void{
+let newsFeed:NewsFeed[] = store.feeds;
 const newsList = [];
 let template = `
- 
+
 <div class="bg-gray-600 min-h-screen">
 <div class="bg-white text-xl">
   <div class="mx-auto px-4">
@@ -53,7 +88,7 @@ let template = `
 </div>
 `;
 if (newsFeed.length === 0){ //데이터가 없다면
-  newsFeed = store.feeds = makeFeeds(getData(NEWS_URL));  //a맨오른쪽 데이터가 왼쪽으로 그게 왼쪽으로 덮어 들어간다. 
+  newsFeed = store.feeds = makeFeeds(getData<NewsFeed[]>(NEWS_URL));  //a맨오른쪽 데이터가 왼쪽으로 그게 왼쪽으로 덮어 들어간다. 
 }
 //아래 삼항연상자 아이번째 뉴스피드의 리드 속성이 참이면 백그라운드 컬러 레드 500 아니면 화이트
 for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
@@ -70,7 +105,7 @@ for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
   <div class="flex mt-3">
     <div class="grid grid-cols-3 text-sm text-gray-500">
       <div><i class="fas fa-user mr-1"></i>${newsFeed[i].user}</div>
-      <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].points}</div>
+      <div><i class="fas fa-heart mr-1"></i>${newsFeed[i].point}</div>
       <div><i class="far fa-clock mr-1"></i>${newsFeed[i].time_ago}</div>
     </div>  
   </div>
@@ -78,18 +113,17 @@ for(let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++){
   `);
 }
 template = template.replace('{{__news_feed__}}', newsList.join(''));
-template = template.replace('{{__prev_page__}}', store.currentPage > 1 ? store.currentPage - 1 : 1);
-template = template.replace('{{__next_page__}}',store.currentPage + 1);
+template = template.replace('{{__prev_page__}}', String(store.currentPage > 1 ? store.currentPage - 1 : 1));
+template = template.replace('{{__next_page__}}',String(store.currentPage + 1));
 
 //if문 쓰기에는 너무짧으니 삼항연산자로 
-
-container.innerHTML= template;
+updateView(template);
 
 }
 
-function newsDetail(){
+function newsDetail():void{
   const id = location.hash.substr(7);
-  const newsContent =getData(CONTENTS_URL.replace('@id', id)); //데이터받기
+  const newsContent =getData<NewsDetail>(CONTENTS_URL.replace('@id', id)); //데이터받기
 
   let template = `
   <div class="bg-gray-600 min-h-screen pb-8">
@@ -127,34 +161,33 @@ for(let i =0; i<store.feeds.length; i++){
   }
 }
 
-function makeComment(comments, called = 0) { //called는 호출된 횟수 , 0으로 기본값 설정
+
+  updateView( template.replace('{{__comments__}}', makeComment(newsContent.comments)));
+}
+
+function makeComment(comments:NewsComment[]):string { //called는 호출된 횟수 , 0으로 기본값 설정
   const commentString = [];
 
   for(let i = 0; i < comments.length; i++) {
+    const comment:NewsComment = comments[i];
     commentString.push(`
-      <div style="padding-left: ${called * 40}px;" class="mt-4">
+      <div style="padding-left: ${comment.level * 40}px;" class="mt-4">
         <div class="text-gray-400">
           <i class="fa fa-sort-up mr-2"></i>
-          <strong>${comments[i].user}</strong> ${comments[i].time_ago}
+          <strong>${comment.user}</strong> ${comment.time_ago}
         </div>
-        <p class="text-gray-700">${comments[i].content}</p>
+        <p class="text-gray-700">${comment.content}</p>
       </div>      
     `);
 
-    if (comments[i].comments.length > 0) {
-      commentString.push(makeComment(comments[i].comments, called + 1)); //재귀호출 makecomments 끝을 알 수 없을 때 유용
+    if (comment.comments.length > 0) {
+      commentString.push(makeComment(comment.comments)); //재귀호출 makecomments 끝을 알 수 없을 때 유용
     }
   }
 
   return commentString.join('');
 }
-
-
-container.innerHTML = template.replace('{{__comments__}}', makeComment(newsContent.comments));
-}
-
-
-function router(){
+function router():void{
   const routePath = location.hash;
   if(routePath === ''){
     newsFeed();
